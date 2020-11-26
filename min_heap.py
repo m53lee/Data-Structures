7d1986cce58371274a90129a31052bce14a2a6c5
Minkyong Lee <m53lee@gmail.com>
2020-11-26 16:24:50 -0500

    complete build_heap method

--- min_heap.py
+++ min_heap.py
@@ -1,7 +1,7 @@
 # Course: CS261 - Data Structures
 # Assignment: 5
 # Student: Minkyong Lee
-# Description: Min Heap Implementation
+# Description: MinHeap Implementation
 
 
 # Import pre-written DynamicArray and LinkedList classes
@@ -51,34 +51,88 @@ class MinHeap:
         """
 
         self.heap.append(node)
-        node_index = self.heap.length()-1
-        parent_index = int((node_index-1) / 2)
+        cur_index = self.heap.length() - 1
+        parent_index = int((cur_index - 1) / 2)
 
         # the added node percolates up the tree until it reaches the root or parent node's value is less
-        while node_index > 0 and self.heap.get_at_index(parent_index) > self.heap.get_at_index(node_index):
-            self.heap.swap(parent_index, node_index)
-            node_index = parent_index
-            parent_index = int((node_index-1) / 2)
+        while cur_index > 0 and self.heap.get_at_index(parent_index) > self.heap.get_at_index(cur_index):
+            self.heap.swap(parent_index, cur_index)
+            cur_index = parent_index
+            parent_index = int((cur_index - 1) / 2)
 
     def get_min(self) -> object:
         """
         Returns an object with a minimum key without removing it from the heap.
         If heap is empty, the method raises an Exception.
+        Runtime complexity must be O(1).
         """
-        return None
+        if self.heap.length() == 0:
+            raise MinHeapException
+
+        return self.heap.get_at_index(0)
 
     def remove_min(self) -> object:
         """
-        TODO: Write this implementation
+        Returns an object with a minimum key and removes it from the heap.
+        If the heap is empty, the method raises an Exception.
+        Runtime complexity of this implementation must be O(logN).
         """
-        return None
+        if self.heap.length() == 0:
+            raise MinHeapException
+
+        min_value = self.heap.get_at_index(0)
+        self.heap.swap(0, self.heap.length() - 1)  # replace the min node with the last filled node
+        self.heap.pop()  # remove the min node
+        self.percolate_down(0)   # call percolate_down helper method
+        return min_value
+
+    def percolate_down(self, cur_index):
+        """
+        Helper method to percolate node down the tree until the subtree is a valid heap
+        Runtime complexity is O(logN)
+        """
+        counter = 0
+        while cur_index >= 0:
+            swap_index = -1
+            right_index = 2 * cur_index + 2
+            left_index = 2 * cur_index + 1
+
+            # if right child is less than the current node
+            if right_index < self.heap.length() and self.heap.get_at_index(right_index) < self.heap.get_at_index(cur_index):
+                if self.heap.get_at_index(left_index) < self.heap.get_at_index(right_index):
+                    swap_index = left_index
+                else:
+                    swap_index = right_index
+            # if left child is less than the current node
+            elif left_index < self.heap.length() and self.heap.get_at_index(left_index) < self.heap.get_at_index(cur_index):
+                swap_index = left_index
+
+            # swap the current node and the smaller child node (swap_index > 0 means that a swap is needed)
+            if swap_index >= 0:
+                self.heap.swap(cur_index, swap_index)
+
+            cur_index = swap_index
 
     def build_heap(self, da: DynamicArray) -> None:
         """
-        TODO: Write this implementation
+        Receives a dynamic array with objects in any order and builds a proper MinHeap from them.
+        Current content of the MinHeap is lost.
+        Runtime complexity must be O(N).
         """
-        pass
+        self.heap = DynamicArray()
+        counter1 = 0
 
+        # copy elements in da to the MinHeap array (O(N))
+        for i in range(da.length()):
+            self.heap.append(da.get_at_index(i))
+
+        last_index = self.heap.length() - 1
+        first_nonleaf = int((last_index - 1) / 2)
+
+        # Start with the first non-leaf node in the tree and continue swapping nodes until
+        # the tree is a valid heap (O(N))
+        for pos in range(first_nonleaf, -1, -1):
+            self.percolate_down(pos)
 
 # BASIC TESTING
 if __name__ == '__main__':
@@ -99,21 +153,18 @@ if __name__ == '__main__':
         h.add(value)
         print(h)
 
-
     print("\nPDF - get_min example 1")
     print("-----------------------")
     h = MinHeap(['fish', 'bird'])
     print(h)
     print(h.get_min(), h.get_min())
 
-
-    # print("\nPDF - remove_min example 1")
-    # print("--------------------------")
-    # h = MinHeap([1, 10, 2, 9, 3, 8, 4, 7, 5, 6])
-    # while not h.is_empty():
-    #     print(h, end=' ')
-    #     print(h.remove_min())
-
+    print("\nPDF - remove_min example 1")
+    print("--------------------------")
+    h = MinHeap([1, 10, 2, 9, 3, 8, 4, 7, 5, 6])
+    while not h.is_empty():
+        print(h, end=' ')
+        print(h.remove_min())
 
     print("\nPDF - build_heap example 1")
     print("--------------------------")
